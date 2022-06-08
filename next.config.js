7226e9427c45f71860bc7c1762866fc0d8d2e5c3
BarakOren <barakoren5@gmail.com>
2022-06-08 23:45:44 +0300

    config update

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
     domains: ['images.unsplash.com']
   },
   env: {
-    FOURSQUERE_AUTH: process.env.FOURSQUERE_AUTH,
-    UNSPLASH_SECRET: process.env.UNSPLASH_SECRET,
-    UNSPLASH_KEY: process.env.UNSPLASH_KEY,
-    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
-    AIRTABLE_BASE_KEY: process.env.AIRTABLE_BASE_KEY
+    FOURSQUERE_AUTH: process.env.NEXT_PUBLIC_FOURSQUERE_AUTH,
+    UNSPLASH_SECRET: process.env.NEXT_PUBLIC_UNSPLASH_SECRET,
+    UNSPLASH_KEY: process.env.NEXT_PUBLIC_UNSPLASH_KEY,
+    AIRTABLE_API_KEY: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+    AIRTABLE_BASE_KEY: process.env.NEXT_PUBLIC_AIRTABLE_BASE_KEY
   }
 }
 
