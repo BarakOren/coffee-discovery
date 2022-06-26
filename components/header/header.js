df0d161291f9172b362fce11ac9dd03b677c8be4
BarakOren <barakoren5@gmail.com>
2022-06-26 23:49:09 +0300

    update

--- components/header/header.js
+++ components/header/header.js
@@ -1,14 +1,6 @@
 import React from "react";
 import styles from "./header.module.css"
-import styled from "styled-components";
-import {CoffeeTogo} from "@styled-icons/boxicons-regular/CoffeeTogo"
 import Link from "next/link"
-
-const CofffeIcon = styled(CoffeeTogo)`
-    width: 30px;
-    height: 30px;
-    color: white;
-`
 
 const Header = () => {
     return(
